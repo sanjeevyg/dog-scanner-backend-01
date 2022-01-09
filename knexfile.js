c90a1464d24c0065f5e251fe3fd894a438d56b39
sanjeevyg <yogi.sjv@gmail.com>
2022-01-08 22:47:02 -0500

    test 01

--- knexfile.js
+++ knexfile.js
@@ -8,35 +8,17 @@ module.exports = {
       filename: 'postgress:///dog-scanner-backend-01'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
     connection: {
       database: process.env.DATABASE_URL,
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'users'
-    }
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   }
 
 };
